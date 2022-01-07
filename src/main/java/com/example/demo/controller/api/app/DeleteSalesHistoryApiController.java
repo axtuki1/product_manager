0fe00035a3d201caab3d73d563d3c1c34c53dee2
axtuki1 <master@axtuki1.com>
2022-01-07 15:18:31 +0900

    販売履歴APIクラスの作成

---
+++ src/main/java/com/example/demo/controller/api/app/DeleteSalesHistoryApiController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller.api.app;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.entity.Item;
+import com.example.demo.form.NewItemInsertForm;
+import com.example.demo.repository.ItemRepository;
+import com.example.demo.repository.SalesDataRepository;
+
+@Controller
+public class DeleteSalesHistoryApiController {
+
+	@Autowired
+	SalesDataRepository repository;
+
+	/**
+	 * 商品を削除するAPI。
+	 * 
+	 * @Endpoint /api/v1/item/{id}
+	 * @Method DELETE
+	 */
+	@RequestMapping(path = "/api/v1/sales/history/{id}", method = RequestMethod.DELETE)
+	@ResponseBody // JSONとしてレスポンスするために使う
+	@CrossOrigin
+	public HashMap<String, Object> viewPage(Model model, HttpSession session,
+			@PathVariable(name = "id") int id) {
+		HashMap<String, Object> data = new HashMap<>();
+		repository.deleteById(id);
+		data.put("result", "ok");
+		return data;
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/api/app/GetSalesHistoryApiController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller.api.app;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.repository.ItemRepository;
+import com.example.demo.repository.SalesDataRepository;
+
+@Controller
+public class GetSalesHistoryApiController {
+
+	@Autowired
+	SalesDataRepository repository;
+
+	/**
+	 * 商品一覧を返すAPI。
+	 * 
+	 * @Endpoint /api/v1/item/{id}
+	 * @Method GET
+	 */
+	@RequestMapping(path = "/api/v1/sales/history/{id}", method = RequestMethod.GET)
+	@ResponseBody // JSONとしてレスポンスするために使う
+	@CrossOrigin
+	public HashMap<String, Object> viewPage(Model model, HttpSession session,
+			@PathVariable(name = "id") int id) {
+		HashMap<String, Object> data = new HashMap<>();
+		data.put("itemData", repository.findById(id));
+		return data;
+	}
+
+}
